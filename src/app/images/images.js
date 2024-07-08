5aed7de7ac9cb2fbb42d7fd382eba26b848722ee
Ashley Vo <ashleyhvo@yahoo.com>
2024-07-07 17:10:31 -0700

    add board

---
+++ src/app/images/images.js
@@ -0,0 +1,11 @@
+import React from "react";
+import img1 from "./images/board.png";
+function Images() {
+  return;
+  <div>
+    <img src={img1} alt="/">
+      {" "}
+    </img>
+  </div>;
+}
+export default Images;
